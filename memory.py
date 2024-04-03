1a7379ccb734c63475ca9f82f3221a3f72fb658f
pigozzif <pigozzife@gmail.com>
2024-04-03 17:16:57 -0400

    fix bugs

--- memory.py
+++ memory.py
@@ -64,12 +64,12 @@ def get_R_US_NS_exhaustive(model, X1, ref, r, scale, k):
 
 def set_UCS_for_R(model, response, stimulus, X1, ref, r, scale, k, reg):
     X2 = simulate_network(model, X1[:, -1], stimulus, r, reg, k)
-    fig = plot_states_trajectory(fig_name="figures/{0}-{1}-{2}.png".format(response, stimulus, int(reg)),
-                                 system_rollout=create_system_rollout_module(grn.config, y0=X1[:, -1]),
-                                 system_outputs=X2,
-                                 observed_node_ids=[response, stimulus],
-                                 observed_node_names={response: "R", stimulus: "ST"})
-    fig.show()
+    # fig = plot_states_trajectory(fig_name="figures/{0}-{1}-{2}.png".format(response, stimulus, int(reg)),
+    #                              system_rollout=create_system_rollout_module(grn.config, y0=X1[:, -1]),
+    #                              system_outputs=X2,
+    #                              observed_node_ids=[response, stimulus],
+    #                              observed_node_names={response: "R", stimulus: "ST"})
+    # fig.show()
     if np.mean(X2.ys[response, :]) >= scale * np.mean(X1[response, :]) and np.mean(
             X2.ys[response, :]) >= scale * np.mean(ref[response, :]):
         return MemoryCircuit(stimulus=stimulus,
@@ -92,34 +92,29 @@ def set_UCS_for_R(model, response, stimulus, X1, ref, r, scale, k, reg):
 
 
 def mem_eval_us_r(model, X1, ref, mem_circuits, r, scale, k):
+    cs_list = [circuit for circuit in mem_circuits if not circuit.is_ucs]
     for ucs_circuit in [circuit for circuit in mem_circuits if circuit.is_ucs]:
-        is_mem, last_genes = test_us_memory(model, X1, ucs_circuit, r, k)
-        if is_mem:
-            cs_list = [circuit for circuit in mem_circuits if not circuit.is_ucs]
-            if cs_list:
-                for cs_circuit in cs_list:
-                    if ucs_circuit.stimulus != cs_circuit.stimulus:
-                        pass
-        else:
-            cs_list = [circuit for circuit in mem_circuits if not circuit.is_ucs]
-            if cs_list:
-                test_pairing_memory(model, X1, ref, ucs_circuit, cs_list, r, scale, k)
+        test_us_memory(model, X1, ucs_circuit, r, k)
+        test_pairing_memory(model, X1, ref, ucs_circuit, cs_list, r, scale, k)
+        test_transfer_memory(model, X1, ucs_circuit, cs_list, r, k)
+        test_associative_memory(model, X1, ref, ucs_circuit, cs_list, r, scale, k)
+        test_consolidation_memory(model, X1, ref, ucs_circuit, cs_list, r, scale, k)
 
 
 def test_us_memory(model, X1, ucs_circuit, r, k):
     e1 = simulate_network(model, X1[:, -1], ucs_circuit.stimulus, r, ucs_circuit.stimulus_reg, k)
-    e2 = model(key=k, y0=e1.ys[:, -1])
+    e2, _ = model(key=k, y0=e1.ys[:, -1])
     return detect_mem(X1, e1, e2, ucs_circuit), e2
 
 
 def test_pairing_memory(model, X1, ref, circuit, cs_list, r, scale, k):
     us_r_cs_genes = []
     for cs in cs_list:
-        e1 = simulate_network(model, X1.ys[:, -1], [circuit.stimulus, cs.stimulus], r,
+        e1 = simulate_network(model, X1[:, -1], [circuit.stimulus, cs.stimulus], r,
                               [circuit.stimulus_reg, cs.stimulus_reg], k)
         up_down_r = detect_reg_r(X1, e1, ref, cs, scale)
         if int(up_down_r) != 0:
-            e2 = model(key=k, y0=e1.ys[:, -1])
+            e2, _ = model(key=k, y0=e1.ys[:, -1])
             is_mem = detect_mem(X1, e1, e2, cs)
             if is_mem:
                 if circuit.stimulus != cs.stimulus:
@@ -131,12 +126,40 @@ def test_pairing_memory(model, X1, ref, circuit, cs_list, r, scale, k):
     return us_r_cs_genes
 
 
+def test_transfer_memory(model, X1, circuit, cs_list, r, k):
+    for cs in cs_list:
+        e1 = simulate_network(model, X1[:, -1], circuit.stimulus, r, circuit.stimulus_reg, k)
+        e2 = simulate_network(model, e1.ys[:, -1], cs.stimulus, r, cs.stimulus_reg, k)
+        is_mem = detect_mem(X1, e1, e2, circuit)
+
+
+def test_associative_memory(model, X1, ref, circuit, cs_list, r, scale, k):
+    for cs in cs_list:
+        e1 = simulate_network(model, X1[:, -1], [circuit.stimulus, cs.stimulus], r,
+                              [circuit.stimulus_reg, cs.stimulus_reg], k)
+        up_down_r = detect_reg_r(X1, e1, ref, cs, scale)
+        if int(up_down_r) != 0:
+            e2 = simulate_network(model, e1.ys[:, -1], cs.stimulus, r, cs.stimulus_reg, k)
+            is_mem = detect_mem(X1, e1, e2, cs)
+
+
+def test_consolidation_memory(model, X1, ref, circuit, cs_list, r, scale, k):
+    for cs in cs_list:
+        e1 = simulate_network(model, X1[:, -1], [circuit.stimulus, cs.stimulus], r,
+                              [circuit.stimulus_reg, cs.stimulus_reg], k)
+        up_down_r = detect_reg_r(X1, e1, ref, cs, scale)
+        if int(up_down_r) != 0:
+            e2 = simulate_network(model, e1.ys[:, -1], cs.stimulus, r, cs.stimulus_reg, k)
+            e3 = simulate_network(model, e2.ys[:, -1], cs.stimulus, r, cs.stimulus_reg, k)
+            is_mem = detect_mem(X1, e1, e3, cs)
+
+
 def detect_reg_r(X1, e1, ref, cs, scale):
     r = cs.response
-    if np.mean(e1.ys[r, :]) >= scale * np.mean(X1.ys[r, :]) \
+    if np.mean(e1.ys[r, :]) >= scale * np.mean(X1[r, :]) \
             and np.mean(e1.ys[r, :]) >= scale * np.mean(ref[r, :]):
         return Regulation(1)
-    elif np.mean(e1.ys[r, :]) <= (1 / scale) * np.mean(X1.ys[r, :]) \
+    elif np.mean(e1.ys[r, :]) <= (1 / scale) * np.mean(X1[r, :]) \
             and np.mean(e1.ys[r, :]) <= (1 / scale) * np.mean(ref[r, :]):
         return Regulation(2)
     return Regulation(0)
@@ -145,8 +168,8 @@ def detect_reg_r(X1, e1, ref, cs, scale):
 def detect_mem(X1, e1, e2, ucs_circuit):
     r = ucs_circuit.response
     if ucs_circuit.response_reg == 1:
-        return np.mean(e2.ys[r, :]) >= np.mean(X1.ys[r, :]) + (np.mean(e1.ys[r, :]) - np.mean(X1.ys[r, :])) / 2.0
-    return np.mean(e2.ys[r, :]) <= np.mean(X1.ys[r, :]) - (np.mean(X1.ys[r, :]) - np.mean(e1.ys[r, :])) / 2.0
+        return np.mean(e2.ys[r, :]) >= np.mean(X1[r, :]) + (np.mean(e1.ys[r, :]) - np.mean(X1[r, :])) / 2.0
+    return np.mean(e2.ys[r, :]) <= np.mean(X1[r, :]) - (np.mean(X1[r, :]) - np.mean(e1.ys[r, :])) / 2.0
 
 
 if __name__ == "__main__":
@@ -159,10 +182,10 @@ if __name__ == "__main__":
 
     grn = GeneRegulatoryNetwork.create(biomodel_idx=model_id)
     reference_output, _ = grn(key=key)
-    fig1 = plot_states_trajectory(fig_name="figures/relax.png",
-                                  system_rollout=create_system_rollout_module(grn.config),
-                                  system_outputs=reference_output)
-    fig1.show()
+    # fig1 = plot_states_trajectory(fig_name="figures/relax.png",
+    #                               system_rollout=create_system_rollout_module(grn.config),
+    #                               system_outputs=reference_output)
+    # fig1.show()
     grn.set_time(n_secs=sim_cnt)
     relax_t = int(sim_cnt / grn.config.deltaT)
     X1 = reference_output.ys[:, :relax_t]
@@ -179,7 +202,7 @@ if __name__ == "__main__":
     # for c in circuits:
     #     print(c)
     # exit()
-    for r in range(len(reference_output)):
+    for r in range(len(reference_output.ys)):
         if circuits[r]:
             mem_eval_us_r(model=grn,
                           X1=X1,
